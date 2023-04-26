4940e6c9ded701a4f6aee09e93ce3dc057b99860
Sathyaprakash Sahoo <62632966+Encryptor-Sec@users.noreply.github.com>
2023-04-27 00:04:33 +0530

    Update lex.py

--- c/lex.py
+++ c/lex.py
@@ -1,3 +1,9 @@
+sudo apt-get update
+sudo apt-get intall flex
+sudo apt-get intall byacc
+sudo apt-get intall bison
+sudo apt-get intall bison++
+sudo apt-get intall byacc-j
 Commadn to run :
 nano filename.l
 lex filename.l (sudo apt-get install flex)
